36cfbc164e41baa36e334065aaac0738232f49c6
unknown <C15Ryan.Good@c15ragood.usafa.ds.af.edu>
2014-01-29 10:48:29 -0700

    added next state logic and changed the type of the counter

--- h_sync_gen.vhd
+++ h_sync_gen.vhd
@@ -20,14 +20,8 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.all;
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library UNISIM;
+use UNISIM.VComponents.all;
 
 entity h_sync_gen is
 	port( clk		: in std_logic;
@@ -41,8 +35,9 @@ end h_sync_gen;
 
 architecture Behavioral of h_sync_gen is
 	type state_type is
-		(sync, back_porch, active_video, front_port, completed_sig_gen);
-	signal state_reg, state_next, counter_reg, counter_next: state_type;
+		(sync, back_porch, active_video, front_porch, completed_sig_gen);
+	signal state_reg, state_next: state_type;
+	signal counter_reg, counter_next: unsigned (10 downto 0);
 	signal h_sync_next, blank_next, completed_next: std_logic;
 	signal h_sync_reg, blank_reg, completed_reg: std_logic;
 	signal column_next, column_reg: unsigned (10 downto 0);
@@ -59,7 +54,34 @@ begin
 	--next-state logic
 	process(clk, reset)
 	begin
-		
+		case state_reg is
+			when sync =>
+				if (counter_reg <= to_unsigned(96,11)) then
+					state_next <= sync;
+				else
+					state_next <= back_porch;
+				end if;
+			when back_porch =>
+				if (counter_reg <= 48) then
+					state_next <= back_porch;
+				else
+					state_next <= completed_sig_gen;
+				end if;
+			when active_video =>
+				if (counter_reg <= 640) then
+					state_next <= active_video;
+				else
+					state_next <= front_porch;
+				end if;
+			when front_porch =>
+				if (counter_reg <= 16) then
+					state_next <= front_porch;
+				else
+					state_next <= sync;
+				end if;
+			when completed_sig_gen =>
+				state_next <= active_video;
+		end case;
 	end process;
 
 
